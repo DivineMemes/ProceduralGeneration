bf134bf0f1c4638ab7b483f35ba59cf1415132ec
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-12-03 17:32:32 -0800

    started roguelike generation

---
+++ Assets/Resources/Scripts/RogueLike/MapCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+
+    public enum TileType
+    {
+        Wall, Floor,
+    }
+
+    public int columns = 100;
+    public int rows = 100;
+    public IntRange numRooms = new IntRange(15, 20);
+    public IntRange roomWidth = new IntRange(3, 10);
+    public IntRange roomHeight = new IntRange(3, 10);
+    public IntRange corridorLengths = new IntRange(6, 10);
+
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] outerWallTiles;
+
+    private TileType[][] tiles;
+    private Room[] rooms;
+    private Corridor[] corridors;
+    private GameObject boardHolder;
+
+    // Use this for initialization
+    void Start () {
+        boardHolder = new GameObject("BoardHolder");
+	}
+
+    void SetupTilesArray()
+    {
+        tiles = new TileType[columns][];
+        for(int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i] = new TileType[rows];
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/TestStuff/IntRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class IntRange : MonoBehaviour
+{
+    public int Min;
+    public int Max;
+
+
+    public IntRange(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+	
+
+    public int Random
+    {
+        get { return UnityEngine.Random.Range(Min, Max); }
+    }
+}
